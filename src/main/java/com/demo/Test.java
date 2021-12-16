e84c3b39832626b02ec40d9428fbfb20bfd7cf4c
Tarun <Tarun@DESKTOP-E1BTVG7>
2021-12-16 23:19:47 +0530

    some edit in controller class

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -1,5 +1,18 @@
 package com.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class Test {
+
+	@RequestMapping("ok")
+	@ResponseBody
+	public String getop()
+	{
+		return "Tarun here hello";
+		
+	}
 
 }
